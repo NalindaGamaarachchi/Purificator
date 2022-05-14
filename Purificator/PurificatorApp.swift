783747c0708d29690b03f6f1aabc0bd5068ca767
NalindaGamaarachchi <44144772+NalindaGamaarachchi@users.noreply.github.com>
2022-05-14 13:08:15 +0530

    Initial Commit

---
+++ Purificator/PurificatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PurificatorApp.swift
+//  Purificator
+//
+//  Created by Nalinda Gamaarachchi on 5/14/22.
+//
+
+import SwiftUI
+
+@main
+struct PurificatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
